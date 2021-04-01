08387f9e678d27d4678e0938a4a5d8cde858045d
AnuV2C18467 <AnuV2C18467@V2CP0081.vee2office.com>
2021-04-01 13:36:09 +0530

    type conversion

--- SchoolApp/src/Widening.java
+++ SchoolApp/src/Widening.java
@@ -1,30 +1,34 @@
-//import java.text.ParseException;
-//import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class Widening {
 	public static void main(String[] args) {
 		int x=200;
-		long y=500;
+		
 		String s="500";
 		
-//		String sDate="20.2.21";
-//		java.util.Date date1=null;
-//		try {
-//			date1=new SimpleDateFormat("dd/mm/yyyy").parse(sDate);
-//		}
-//		catch(ParseException e){
-//			e.printStackTrace();
-//		}
-//		System.out.println(date1);
-//		
+		String sdate="Fri, 04/03/2021, 23:37:50";
+		Date date1=null;
+		try
+		{
+			date1=new SimpleDateFormat("E, dd/MM/YYYY, HH:MM:SS").parse(sdate);
+		}
+		catch(ParseException e)
+		{
+			System.out.println(e);
+		}
 		
+		System.out.println(date1);
+			
 		
 		int i = Integer.parseInt(s);
 		int j = Integer.valueOf(s);
 		String k= String.valueOf(x);
 		String l= Integer.toString(x);
-		//long n = Long.parselong(y);
-		long m = Long.valueOf(y);
+//		long n = Long.parseLong(x);
+		long m = Long.valueOf(x);
 		
 		
 		
@@ -32,7 +36,7 @@ public class Widening {
 		System.out.println("String to integer conversion using valueOf : "+j);
 		System.out.println("Integer to String conversion using valueOf : "+k);
 		System.out.println("Integer to String conversion using toString : "+l);
-		System.out.println("Integer to String conversion using toString : "+m);
-		//System.out.println("Integer to String conversion using toString : "+n);
+		System.out.println("Integer to Long conversion using valueOf : "+m);
+//		System.out.println("Integer to Long conversion using parseLong : "+n);
 	}
 }
